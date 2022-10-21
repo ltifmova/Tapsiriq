f98b80f9200a423f88f3a6d9e9b3c88bc8baba29
Amin Memmedli <sxlops@Fxlops>
2022-10-22 00:11:29 +0400

    Tapşırıq1 repostryə əlavə edili

---
+++ JavaScript_Tapsiriqlari/tapsiriq1.js
@@ -0,0 +1,12 @@
+function minimumElement(array) {
+    let max = array[0]
+    for (let i = 0; i < array.length; i++) {
+      if (max < array[i]) {
+        max = array[i]
+      }
+    }
+    return max
+  }
+  
+  const sample_array = [1, 2, 4, 6, 71, 2, 4, 9, 100];
+  console.log(minimumElement(sample_array));
