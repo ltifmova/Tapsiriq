6eb0c47447bf08d0a0a95003d1c47637b59f9794
Amin Memmedli <sxlops@Fxlops>
2022-10-22 16:46:45 +0400

    Tapsiriq 3 və 4 repostry`ə əlavə edildi.

---
+++ JavaScript_Tapsiriqlari/tapsiriq3.js
@@ -0,0 +1,14 @@
+function sum(array) {
+    
+    let s=0;
+    for (let i=0; i<array.length; i++){
+      sample_array.forEach(number => s=s+number );
+     break ;
+     console.log(s);
+       
+    }
+    return s;
+}
+
+const sample_array=[1,2,4,6,71,2,4,9,100];
+console.log(sum(sample_array));
