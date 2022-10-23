99057aff9f2b58defddbae07bbd0915124e25157
Amin Memmedli <fxlops@Fxlops>
2022-10-23 14:46:27 +0400

    Facebook üçün react application yaradilib repostryə əlavə edildi.

---
+++ Facebook_React/react_facebook/src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+<div class="eyvALLAH">
+            <div class="Fbwords">
+                <h1>
+                    facebook
+                </h1>
+                <h2>
+                    Connect with friends and the world around you on Facebook.
+                </h2>
+            </div>
+            <div class="container">
+                <form class="form">
+                    <div class="inputs">
+                        <input type="text" class="textone" placeholder="Email or phone number"></input>
+                        <br></br>
+                        <br></br>
+                        <input type="password" class="texttwo" placeholder="Password"></input>
+                    </div>
+                    <br></br>
+                    <button type="submit" class="textthree"><a href="#" class="textthree">Log In</a></button>
+                    <br></br>
+                    <br></br>
+                    <a href="https://www.facebook.com/recover/initiate/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNjY0NzIzMTAyLCJjYWxsc2l0ZV9pZCI6MzgxMjI5MDc5NTc1OTQ2fQ%3D%3D&ars=facebook_login"
+                        class="textsix">Forgot password?</a>
+                    <br></br>
+                    <hr class="textfour"></hr>
+                    <button type="submit" class="textfive"><a href="#" class="textfive">Create a new
+                        account</a></button>
+                </form>
+
+                <div>
+                    <a href="https://www.facebook.com/pages/create/?ref_type=registration_form" class="textseven">Create a
+                        Page </a>
+                     for a celebrity, brand or business.
+                </div>
+            </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div id="footer_wrapper">
+            <div id="footer1">
+                English (US) <a href="">Türkçe</a><a href="">Azərbaycan dili</a><a href=""> Русский</a><a href="">العربية</a><a
+                    href="">Deutsch</a><a href="">Français (France)</a><a href="">فارسی</a><a href="">ქართული</a><a
+                        href="">Español</a><a href="">Português (Brasil)</a>
+            </div>
+            <div id="footer2">
+                <a href="">Sign Up</a><a href="">Log In</a><a href="">Messenger</a><a href="">Facebook Lite</a><a
+                    href="">Watch</a><a href="">Places</a><a href="">Games</a><a href="">Marketplace</a><a href="">Meta
+                        Pay</a><a href="">Oculus</a><a href="">Portal</a><a href="">Instagram</a><a href="">Bulletin</a><a
+                            href="">Local</a><a href="">Fundraisers</a><a href="">Services</a><a href="">Voting Information
+                                Center</a><a href="">Groups</a><a href="">About</a><a href="">Create Ad</a><a href="">Create Page</a><a
+                                    href="">Developers</a><a href="">Careers</a><a href="">Privacy</a><a href="">Cookies</a><a href="">Ad
+                                        choices</a><a href="">Terms</a><a href="">Help</a><a href="">Contact Uploading & Non-UsersSettings</a>
+                <br></br>
+                <p>Meta © 2022</p>
+                <br></br>
+                <br></br>
+                <br></br>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default App;
